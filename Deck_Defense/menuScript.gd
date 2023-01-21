65edb33ca5fb144df5909c0b39e188d6149cec12
vizualjack <67346906+vizualjack@users.noreply.github.com>
2023-01-21 19:54:57 +0100

    the beginning

---
+++ Deck_Defense/menuScript.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Game.tscn")
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
