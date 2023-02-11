617f97ac90c336c81eae43a88665e4d4bea200ad
vizualjack <67346906+vizualjack@users.noreply.github.com>
2023-02-11 20:21:45 +0100

    packages

--- Deck_Defense/scripts/packages.gd
+++ Deck_Defense/scripts/packages.gd
@@ -1,30 +1,42 @@
 extends Control
 
 
+var pointsLabel:Label
 var packageHolder
 var packageScene:PackedScene
-
+var points = 1000
+var packages = []
 
 
 func _ready():
 	packageHolder = get_node("scrollWrapper/packageHolder")
+	pointsLabel = get_node("points")
 	packageScene = load("res://prefabs/package.tscn")
 	_loadPackages()
+	_refreshPoints()
+
+
+func _refreshPoints():
+	pointsLabel.text = String(points) + " Pt"
 
 
 func _loadPackages():
+	var rng = RandomNumberGenerator.new()
 	for i in 10:
-		var newPackage = packageScene.instance()
-		var label = newPackage.get_node("name") as Label
-		label.text = "Moooin " + String(i)
-		var buyButton = newPackage.get_node("bar/buyButton") as Control
-		buyButton.connect("gui_input",self,"onBuyButton", [i])
+		var newPackage = packageScene.instance() as Package
+		var name = "Moin" + String(i)
+		var price = rng.randi_range(1,10)
+		newPackage.setName(name)
+		newPackage.setPrice(price)
+		newPackage.setOnClick(self, "onBuyButton", i)
 		packageHolder.add_child(newPackage)
+		packages.append(newPackage)
 		
 
-func onBuyButton(event, packageIndex):
-	if event is InputEventMouseButton and event.pressed:
-		print("buying..." + String(packageIndex))
+func onBuyButton(packageIndex):
+	var selectedPackage = packages[packageIndex] as Package
+	points -= selectedPackage.price
+	_refreshPoints()
 
 
 func _on_back_gui_input(event):
