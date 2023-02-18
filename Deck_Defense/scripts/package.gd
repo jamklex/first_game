d92b8c175046b90519786cff16a5ae1768e28e32
vizualjack <67346906+vizualjack@users.noreply.github.com>
2023-02-18 20:49:19 +0100

    booster pack

--- Deck_Defense/scripts/package.gd
+++ Deck_Defense/scripts/package.gd
@@ -6,15 +6,28 @@ class_name Package
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+enum Style {Available, NotEnoughMoney, Sold}
+
+var style = Style.Available
 var price
+var numberOfPacks = 10
 var funcObj:Control
 var functionName
 var functionArg
 
+var buyBtnLabel:Label
+var buyBtn:Panel
+var buyBtnStyle = StyleBoxFlat.new()
+var availableColor = Color8(189,138,0)
+var notAvailableColor = Color8(20,20,20)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	buyBtnLabel = get_node("bar/buyButton/Label") as Label
+	buyBtn = get_node("bar/buyButton") as Panel
+	buyBtnStyle.bg_color = availableColor
+	buyBtn.add_stylebox_override("panel", buyBtnStyle)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,13 +42,34 @@ func setPrice(newPrice):
 	label.text = String(newPrice) + " Pt"
 	price = newPrice
 	
+	
+func setCover(imagePath):
+	var texture = load(imagePath) as Texture
+	var packHolder = get_node("packs") as Control
+	for children in packHolder.get_children():
+		var child = children as Sprite
+		child.texture = texture
 
 func setOnClick(target, funcName, arg):
 	funcObj = target
 	functionName = funcName
 	functionArg = arg
+	
+	
+func setStyle(newStyle):
+	style = newStyle
+	if style == Style.Available:
+		buyBtnLabel.text = "Buy"
+		buyBtnStyle.bg_color = availableColor
+	else:
+		if style == Style.NotEnoughMoney:
+			buyBtnLabel.text = "Buy"
+		else:
+			buyBtnLabel.text = "Sold"
+		buyBtnStyle.bg_color = notAvailableColor
+	buyBtn.update()
 
 
 func _on_buyButton_gui_input(event):
-	if event is InputEventMouseButton and event.pressed:
+	if style == Style.Available and event is InputEventMouseButton and event.pressed:
 		funcObj.call(functionName, functionArg)
--- Deck_Defense/scripts/packages.gd
+++ Deck_Defense/scripts/packages.gd
@@ -4,28 +4,51 @@ extends Control
 var pointsLabel:Label
 var packageHolder
 var packageScene:PackedScene
-var points = 1000
+var cardScene:PackedScene
+var points = 200
 var packages = []
+
+var resultWindow:Panel
+var resultWindowCards:GridContainer
+
+var rng = RandomNumberGenerator.new()
 
 
 func _ready():
+	rng.randomize()
 	packageHolder = get_node("scrollWrapper/packageHolder")
 	pointsLabel = get_node("points")
+	resultWindow = get_node("resultWindow")
+	resultWindowCards = get_node("resultWindow/scrollWrapper/cardHolder")
 	packageScene = load("res://prefabs/package.tscn")
+	cardScene = load("res://prefabs/card.tscn")
 	_loadPackages()
 	_refreshPoints()
 
 
 func _refreshPoints():
 	pointsLabel.text = String(points) + " Pt"
+	
+	
+func _refreshPackages():
+	for pack in packages:
+		pack = pack as Package
+		if pack.numberOfPacks == 0:
+			pack.setStyle(Package.Style.Sold)
+		elif points < pack.price:
+			pack.setStyle(Package.Style.NotEnoughMoney)
+		else:
+			pack.setStyle(Package.Style.Available)
 
 
 func _loadPackages():
-	var rng = RandomNumberGenerator.new()
 	for i in 10:
 		var newPackage = packageScene.instance() as Package
 		var name = "Moin" + String(i)
 		var price = rng.randi_range(1,10)
+		var coverIndex = String(rng.randi_range(1,3))
+		var imagePath = "res://images/packCovers/cover" + coverIndex + ".png"
+		newPackage.setCover(imagePath)
 		newPackage.setName(name)
 		newPackage.setPrice(price)
 		newPackage.setOnClick(self, "onBuyButton", i)
@@ -36,9 +59,22 @@ func _loadPackages():
 func onBuyButton(packageIndex):
 	var selectedPackage = packages[packageIndex] as Package
 	points -= selectedPackage.price
+	selectedPackage.numberOfPacks -= 1
 	_refreshPoints()
+	_refreshPackages()
+	for child in resultWindowCards.get_children():
+		resultWindowCards.remove_child(child)
+	for i in rng.randi_range(1,10):
+		var newCard = cardScene.instance()
+		resultWindowCards.add_child(newCard)
+	resultWindow.visible = true
 
 
 func _on_back_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		get_tree().change_scene("res://scenes/Menu.tscn")
+
+
+func _on_close_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		resultWindow.visible = false
