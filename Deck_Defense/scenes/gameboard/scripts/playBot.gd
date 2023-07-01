25d72b34e688b9ef79a37cd3034632e3114f78f1
Maximilian Rich <maximilian.rich@online.de>
2023-07-01 20:37:04 +0200

    remove comments

--- Deck_Defense/scenes/gameboard/scripts/playBot.gd
+++ Deck_Defense/scenes/gameboard/scripts/playBot.gd
@@ -13,21 +13,16 @@ static func of(level):
 	return enemy
 
 func play_move(hand_container: HBoxContainer, my_cards_container: HBoxContainer, opponent_cards_container: HBoxContainer, opponent_health_node: Node):
-	print("\nnext turn")
 	var hand_cards_amount = hand_container.get_child_count()
 	var cards_that_can_attack_count = GbUtil.count_attacking_cards(my_cards_container)
 	var attack_when_at_least = rng.randi_range(2, GbProps.max_card_space_spots - level)
 	var attack_instead = false
-	print("will only attack when " + str(attack_when_at_least) + " cards can attack")
 	if hand_cards_amount > 0 and attack_when_at_least >= cards_that_can_attack_count:
 		var playable_cards = GbUtil.cards_ltr_in(hand_container) as Array
 		var cards_to_play = rng.randi_range(1, min(5, playable_cards.size()))
-		print("will play " + str(cards_to_play) + " cards")
 		for i in range(cards_to_play):
-			print("card #" + str(i+1))
 			var placement_scored = placements(playable_cards, card_space_dict(my_cards_container), card_space_dict(opponent_cards_container))
 			if placement_scored.values().max() < 1:
-				print("nevermind, that would be stupid!")
 				if i == 0 and cards_that_can_attack_count > 0:
 					attack_instead = true
 					break
@@ -38,7 +33,6 @@ func play_move(hand_container: HBoxContainer, my_cards_container: HBoxContainer,
 			playable_cards.remove_at(playable_cards.find(placement.card))
 		if not attack_instead:
 			return true
-	print("attacking")
 	return await GbUtil.attack(GbUtil.cards_ltr_in(my_cards_container), GbUtil.cards_ltr_in(opponent_cards_container), opponent_health_node, true)
 
 func placements(cards: Array, my_field: Dictionary, opponent_field: Dictionary):
@@ -57,7 +51,6 @@ func random_placement(diff: int, placements_scored: Dictionary):
 		if placements_scored[i] >= min_value:
 			placements_to_choose.append(i)
 	var placement = placements_to_choose.pick_random()
-	print(str(min_value) + " - " + str(max_value) + " -> " + str(placements_scored[placement]))
 	return placement
 
 func card_space_dict(cards_container: HBoxContainer):
